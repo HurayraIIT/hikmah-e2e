19058280768329b17553f11a0e0f324c7dd3f5ac
Abu Hurayra <hurayraiit@gmail.com>
2025-04-20 19:39:57 +0600

    added more tests and updated the yml file

---
+++ tests/dashboard.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from '@playwright/test';
+import { randomSlug, randomString } from '../utils/random-data';
+
+const hikmahAuthFile = `playwright/.auth/hikmah01.json`;
+
+test.describe.configure({ mode: 'serial' });
+
+test("hikmah - authenticated - CRUD Post", async ({ browser }) => {
+    const context = await browser.newContext({ storageState: hikmahAuthFile });
+    const page = await context.newPage();
+    await page.goto(`${process.env.BASE_URL}`);
+    const postContent = randomSlug();
+
+    // Ensure user is logged in
+    await expect.soft(page.locator('#sidebarUserMenu').getByText('Abu Hurayra')).toBeVisible();
+    await page.locator('#sidebarUserMenu').getByText('Abu Hurayra').click();
+
+    await page.locator('#top').getByText('@hurayraiit').waitFor();
+    await expect.soft(page.locator('#top').getByText('@hurayraiit')).toBeVisible();
+
+    // Create a post
+    await page.getByRole('button', { name: 'Something nice to share?' }).click();
+    await expect.soft(page.locator('div').filter({ hasText: /^Create post$/ })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Something nice to share?' }).fill(`### ${postContent}`);
+    await expect.soft(page.getByRole('button', { name: 'Post', exact: true })).toBeVisible();
+    await page.getByRole('button', { name: 'Post', exact: true }).click();
+    await expect.soft(page.getByText('Your post is submitted for review.')).toBeVisible();
+    await expect.soft(page.getByText('It will be published after some safety checks')).toBeVisible();
+    await expect.soft(page.getByRole('heading', { name: postContent })).toBeVisible();
+
+    // Now delete post
+    await page.locator('.pt-\\[0\\.1rem\\]').first().click();
+    await expect.soft(page.getByRole('button', { name: 'Delete Post' })).toBeVisible();
+    await expect.soft(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
+    await page.getByRole('button', { name: 'Delete Post' }).click();
+    await expect.soft(page.locator('div').filter({ hasText: /^Are you sure you want to delete this post\?$/ })).toBeVisible();
+    await page.getByRole('button', { name: 'Ok' }).click();
+    await page.waitForTimeout(500);
+    await expect.soft(page.getByRole('heading', { name: postContent })).not.toBeVisible();
+});
+
+test("hikmah homepage - authenticated infinite scrolling", async ({ browser }) => {
+    const context = await browser.newContext({ storageState: hikmahAuthFile });
+    const page = await context.newPage();
+    await page.goto(`${process.env.BASE_URL}`);
+    await page.waitForLoadState('domcontentloaded');
+
+    const feedItems = page.locator('div.feed-item');
+
+    // Test infinite scroll with for loop
+    for (let i = 0; i < 35; i += 1) {
+        // scroll
+        if (await feedItems.nth(i).isVisible()) {
+            await feedItems.nth(i).scrollIntoViewIfNeeded();
+            // Print author name
+            // console.log(`${i}: ${await feedItems.nth(i).locator("a.base-username").first().textContent()}`);
+        } else {
+            await page.locator("div.v3-infinite-loading").nth(1).scrollIntoViewIfNeeded();
+            await page.waitForTimeout(1500);
+        }
+
+        await page.waitForTimeout(500);
+    }
+
+    // moderation status strings should not be visible in the page
+    await expect.soft(page.getByText("Someone in our team is checking your content...")).not.toBeVisible();
+    await expect.soft(page.getByText("Rejected for having harmful content")).not.toBeVisible();
+});
--- tests/generate-session.spec.ts
+++ tests/generate-session.spec.ts
@@ -7,7 +7,7 @@ const hikmahAuthFile = `playwright/.auth/hikmah01.json`;
 const username = `${process.env.LINKEDIN_EMAIL}`;
 const password = `${process.env.LINKEDIN_PASS}`;
 
-test('authenticate kahf id and save the session', async ({ page }) => {
+test.skip('authenticate kahf id and save the session', async ({ page }) => {
     await page.goto(`${process.env.BASE_URL}`);
     await expect(page).toHaveTitle(/Hikmah/);
     await expect(page.getByText('Welcome to Hikmah')).toBeVisible();
@@ -27,7 +27,7 @@ test('authenticate kahf id and save the session', async ({ page }) => {
     await page.context().storageState({ path: kahfIdAuthFile });
 });
 
-test("login to hikmah and save the session", async ({ browser }) => {
+test.skip("login to hikmah and save the session", async ({ browser }) => {
     const context = await browser.newContext({ storageState: kahfIdAuthFile });
     const page = await context.newPage();
     await page.goto(`${process.env.KAHF_ID}`);
@@ -48,66 +48,4 @@ test("login to hikmah and save the session", async ({ browser }) => {
     await page.getByRole('button', { name: '+ Add New Post' }).click();
 
     // await page.context().storageState({ path: hikmahAuthFile });
-});
-
-test("hikmah - authenticated - CRUD Post", async ({ browser }) => {
-    const context = await browser.newContext({ storageState: hikmahAuthFile });
-    const page = await context.newPage();
-    await page.goto(`${process.env.BASE_URL}`);
-    const postContent = randomSlug();
-
-    // Ensure user is logged in
-    await expect.soft(page.locator('#sidebarUserMenu').getByText('Abu Hurayra')).toBeVisible();
-    await page.locator('#sidebarUserMenu').getByText('Abu Hurayra').click();
-
-    await page.locator('#top').getByText('@hurayraiit').waitFor();
-    await expect.soft(page.locator('#top').getByText('@hurayraiit')).toBeVisible();
-
-    // Create a post
-    await page.getByRole('button', { name: 'Something nice to share?' }).click();
-    await expect.soft(page.locator('div').filter({ hasText: /^Create post$/ })).toBeVisible();
-    await page.getByRole('textbox', { name: 'Something nice to share?' }).fill(`### ${postContent}`);
-    await expect.soft(page.getByRole('button', { name: 'Post', exact: true })).toBeVisible();
-    await page.getByRole('button', { name: 'Post', exact: true }).click();
-    await expect.soft(page.getByText('Your post is submitted for review.')).toBeVisible();
-    await expect.soft(page.getByText('It will be published after some safety checks')).toBeVisible();
-    await expect.soft(page.getByRole('heading', { name: postContent })).toBeVisible();
-
-    // Now delete post
-    await page.locator('.pt-\\[0\\.1rem\\]').first().click();
-    await expect.soft(page.getByRole('button', { name: 'Delete Post' })).toBeVisible();
-    await expect.soft(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
-    await page.getByRole('button', { name: 'Delete Post' }).click();
-    await expect.soft(page.locator('div').filter({ hasText: /^Are you sure you want to delete this post\?$/ })).toBeVisible();
-    await page.getByRole('button', { name: 'Ok' }).click();
-    await page.waitForTimeout(500);
-    await expect.soft(page.getByRole('heading', { name: postContent })).not.toBeVisible();
-});
-
-test("hikmah homepage - authenticated infinite scrolling", async ({ browser }) => {
-    const context = await browser.newContext({ storageState: hikmahAuthFile });
-    const page = await context.newPage();
-    await page.goto(`${process.env.BASE_URL}`);
-    await page.waitForLoadState('domcontentloaded');
-
-    const feedItems = page.locator('div.feed-item');
-
-    // Test infinite scroll with for loop
-    for (let i = 0; i < 35; i += 1) {
-        // scroll
-        if (await feedItems.nth(i).isVisible()) {
-            await feedItems.nth(i).scrollIntoViewIfNeeded();
-            // Print author name
-            // console.log(`${i}: ${await feedItems.nth(i).locator("a.base-username").first().textContent()}`);
-        } else {
-            await page.locator("div.v3-infinite-loading").nth(1).scrollIntoViewIfNeeded();
-            await page.waitForTimeout(1500);
-        }
-
-        await page.waitForTimeout(500);
-    }
-
-    // moderation status strings should not be visible in the page
-    await expect.soft(page.getByText("Someone in our team is checking your content...")).not.toBeVisible();
-    await expect.soft(page.getByText("Rejected for having harmful content")).not.toBeVisible();
 });
